ac6a3ec2efc3f029366ebc38f4c1b1752f6ee93d
odp108109 <odp108109@APHDIR108109.odontoprev.com.br>
2018-06-15 19:07:40 -0300

    Vue.JS

--- teste.php
+++ teste.php
@@ -3,11 +3,31 @@
 <html>
 <head>
 	<title></title>
+	<script src="./script/vue.js"></script>
+    <script>
+	function myFunction() {
+	new Vue({
+		  el: '#app',
+		  data: {
+		    message: 'Hello Vue.js by Marcelo!'
+		  }
+		});
+	}
+	</script>
 </head>
-<body>
-<?php
-	echo("Hello World 2!!!");
-?>
+<body onload="myFunction()">
+	<?php
+		echo("Hello World 2 !!!");
+	?>
+    <p>Testing Vue.JS<p>
+        
+    <div id="app">
+        <p>{{ message }}</p>
+    </div>
+    <?php     
+        //phpinfo();
+	?>
 </body>
-
 </html>
+
+
