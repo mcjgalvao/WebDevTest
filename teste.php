15a6b812238c795c1188a14d2ff38f613f629fb6
user <user@Dexter64>
2018-06-17 22:08:41 -0300

    Javascript tutorial JS Array Methods

--- teste.php
+++ teste.php
@@ -3,30 +3,66 @@
 <html>
 <head>
 	<title></title>
+    <script src="./script/jquery-3.3.1.js"></script>
 	<script src="./script/vue.js"></script>
     <script>
-	function myFunction() {
-	new Vue({
-		  el: '#app',
-		  data: {
-		    message: 'Hello Vue.js by Marcelo!'
-		  }
-		});
+    $(document).ready(function(){
+    	new Vue({
+  		  el: '#app',
+  		  data: {
+  		    message: 'Hello Vue.js by Marcelo!'
+  		  }
+  	  		});
+  		new Vue({
+  		  el: '#app1',
+  		  data: {
+  		    msg: 'Vue.js'
+  		  }
+  		});
+        $("p").click(function(){
+            $(this).hide();
+        });
+    });
+    </script>
+	<style>
+	.vue-js {
+	   color: red;
 	}
-	</script>
+	.php {
+	   color: blue;
+	}
+	</style>
 </head>
-<body onload="myFunction()">
-	<?php
-		echo("Hello World 2 !!!");
-	?>
-    <p>Testing Vue.JS<p>
+<body>
+
+    <p>Testing 
+    <span id="app1" class="vue-js">
+        {{ msg }}
+    </span>
+    and
+    <span class="php">
+    	<?php
+		  echo("PHP !!!");
+	   ?>
+    </span>
+    <p>
         
-    <div id="app">
+    <div id="app" class="vue-js">
         <p>{{ message }}</p>
+    </div>
+    
+    <button type="button" onclick='document.getElementById("teste").innerHTML = "&lt;p&gt;Hello JavaScript!&lt;/p&gt;"'>Click Me!</button>
+    
+    <div id="teste">
     </div>
     <?php     
         //phpinfo();
 	?>
+	
+	<script>
+		console.log(5 + 6);
+		console.log("Testando...");
+	</script>
 </body>
 </html>
 
